67f89ebf9f3428ff55b3c2e00075d4614d447e42
olgaodc <olgaodincova@gmail.com>
2023-06-30 15:10:29 +0300

    navbar and hero box added

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './styles.module.css';
+// import logoImage from '../../assets/';
 
 const Navbar = () => {
   return (
     <div className={styles.navbarWrapper}>
         <div className={styles.container}>
             <div className={styles.navbar}>
-                <Link href={'/'}>
-                    {/* <img/> */}
-                    <span>DevHelp</span>
+                <Link className={styles.logoWrapper} href={'/'}>
+                    <img className={styles.logoImage} src={'https://cdn0.iconfinder.com/data/icons/computer-223/24/question_support_information_service_advice_inquiry-25-512.png'}/>
+                    <span className={styles.logoText}>DevHelp</span>
                 </Link>
+                <ul className={styles.navbarList}>
+                  <li className={styles.navbarListItem}>
+                    <Link className={styles.itemLink} href={'/'}>Home</Link>
+                  </li>
+                  <li className={styles.navbarListItem}>
+                    <Link className={styles.itemLink} href={'/'}>Log in</Link>
+                  </li>
+                  <li className={styles.navbarListItem}>
+                    <Link className={styles.itemLink} href={'/'}>Sign up</Link>
+                  </li>
+                </ul>
             </div>
         </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Navbar from '../components/navbar/navbar';
 import styles from './styles.module.css';
+import heroImage from '../assets/layered-steps-haikei.svg';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <>
-      <Navbar/>
-      <div>Main page</div>
-    </>
+
+    <div className={styles.contentWrapper}>
+      <Navbar />
+      <div className={styles.heroBoxWrapper}>
+        <div className={styles.container}>
+          <div className={styles.heroBox}>
+            <img className={styles.heroImage} src={heroImage.src}></img>
+            <h1 className={styles.heroBoxTitle}>A public platform building the definitive collection of coding questions & answers</h1>
+            <p className={styles.heroBoxDescription}>A community-based space to find and contribute answers to technical challenges. Whether you're a seasoned developer, a curious learner, or an expert in your field, our platform provides a vibrant community where you can connect with like-minded individuals to tackle technical hurdles. Together, we empower each other to overcome technical challenges and foster continuous learning in the ever-evolving world of technology.</p>
+            <Link className={styles.heroBoxButton} href={'/'}>Sign up</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
